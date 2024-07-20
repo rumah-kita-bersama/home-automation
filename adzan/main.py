5053d7e48f70f723f99e7fe2783f7c025bd8d873
Anonymous <>
2024-07-20 12:19:32 +0800

    update adzan

--- adzan/main.py
+++ adzan/main.py
@@ -1,59 +1,72 @@
 import crontab
 import requests
 import bs4
+import time
 
-PLAY_ADZAN_COMMAND = 'mpg321 /home/pi/home-automation/adzan/adzan.mp3 >> /dev/null 2>&1'
-PRAYER_WHITELIST = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']
+PLAY_ADZAN_COMMAND = "mpg321 /home/pi/home-automation/adzan/adzan.mp3 >> /dev/null 2>&1"
+PRAYER_WHITELIST = ["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"]
 MUIS_PRAYER_LIST = [
-    {
-        'name': 'Subuh',
-        'hour_offset': 0
-    },
-    {
-        'name': 'Zohor',
-        'hour_offset': 12
-    },
-    {
-        'name': 'Asar',
-        'hour_offset': 12
-    },
-    {
-        'name': 'Maghrib',
-        'hour_offset': 12
-    },
-    {
-        'name': 'Isyak',
-        'hour_offset': 12
-    },
+    {"name": "Subuh", "hour_offset": 0},
+    {"name": "Zohor", "hour_offset": 12},
+    {"name": "Asar", "hour_offset": 12},
+    {"name": "Maghrib", "hour_offset": 12},
+    {"name": "Isyak", "hour_offset": 12},
 ]
 
 
 def get_prayer_time() -> dict:
     response = requests.get(
-        'https://api.pray.zone/v2/times/today.json', {"city": "singapore", "school": 10}).json()
+        "https://api.pray.zone/v2/times/today.json", {"city": "singapore", "school": 10}
+    ).json()
 
-    if response['status'] == 'OK':
-        prayer_times = response['results']['datetime'][0]['times']
-        return {prayer: time for prayer, time in prayer_times.items() if prayer in PRAYER_WHITELIST}
+    if response["status"] == "OK":
+        prayer_times = response["results"]["datetime"][0]["times"]
+        return {
+            prayer: time
+            for prayer, time in prayer_times.items()
+            if prayer in PRAYER_WHITELIST
+        }
     return dict()
 
 
+# def get_muis_prayer_time() -> dict:
+#     response = requests.get("https://www.muis.gov.sg/")
+#     result = {}
+
+#     if response.status_code != 200:
+#         return result
+
+#     print(response.content)
+#     soup = bs4.BeautifulSoup(response.content, features="html.parser")
+#     for prayer in MUIS_PRAYER_LIST:
+#         element = soup.find("span", {"id": f'PrayerTimeControl1_{prayer["name"]}'})
+
+#         print(element)
+
+#         if element is not None:
+#             hour, minute = element.string.split(":")
+#             hour = str((int(hour) + prayer["hour_offset"]) % 24)
+#             result[prayer["name"]] = f"{hour}:{minute}"
+
+
+#     return result
 def get_muis_prayer_time() -> dict:
-    response = requests.get('https://www.muis.gov.sg/')
-    result = {}
+    response = requests.get(
+        f"https://www.muis.gov.sg/api/pagecontentapi/GetPrayerTime?v={time.time() * 1000}"
+    )
+    result = dict()
 
     if response.status_code != 200:
         return result
 
-    soup = bs4.BeautifulSoup(response.content, features="html.parser")
+    schedule = response.json()
     for prayer in MUIS_PRAYER_LIST:
-        element = soup.find(
-            'span', {'id': f'PrayerTimeControl1_{prayer["name"]}'})
+        if prayer["name"] not in schedule:
+            continue
 
-        if element is not None:
-            hour, minute = element.string.split(':')
-            hour = str((int(hour) + prayer['hour_offset']) % 24)
-            result[prayer['name']] = f'{hour}:{minute}'
+        hour, minute = schedule[prayer["name"]].split(":")
+        hour = str((int(hour) + prayer["hour_offset"]) % 24)
+        result[prayer["name"]] = f"{hour}:{minute}"
 
     return result
 
@@ -81,7 +94,7 @@ def set_adzan_cronjob(cron: crontab.CronTab) -> None:
 
 
 def main():
-    cron = crontab.CronTab(user='pi')
+    cron = crontab.CronTab(user="pi")
 
     # Remove (possibly) outdated adzan time
     remove_cronjobs_by_command(cron, PLAY_ADZAN_COMMAND)
@@ -89,5 +102,5 @@ def main():
     set_adzan_cronjob(cron)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
